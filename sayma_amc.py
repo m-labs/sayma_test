c35fb8b10fb62b8a204fb41c340149ec5fee0c5b
Florent Kermarrec <florent@enjoy-digital.fr>
2017-05-29 16:32:21 +0200

    sayma_amc: add AMCRTMLinkTestSoC

--- sayma_amc.py
+++ sayma_amc.py
@@ -3,6 +3,7 @@ import sys
 sys.path.append("gateware") # FIXME
 
 from litex.gen import *
+from litex.soc.interconnect.csr import *
 from litex.gen.genlib.resetsync import AsyncResetSynchronizer
 
 from litex.build.generic_platform import *
@@ -23,6 +24,9 @@ from litejesd204b.core import LiteJESD204BCoreTX
 from litejesd204b.core import LiteJESD204BCoreTXControl
 
 from transceiver.gth_ultrascale import GTHChannelPLL, GTH
+from transceiver.serdes_ultrascale import SERDESPLL, SERDES
+
+from litescope import LiteScopeAnalyzer
 
 
 _io = [
@@ -146,6 +150,17 @@ _io = [
         Subsignal("n", Pins("AM1"))
     ),
     ("sfp_tx_disable_n", 1, Pins("AM12"), IOStandard("LVCMOS18")),
+
+    # amc_rtm_link
+    ("amc_rtm_link", 1,
+        Subsignal("clk_p", Pins("FIXME"), Misc("DIFF_TERM=TRUE")),
+        Subsignal("clk_n", Pins("FIXME"), Misc("DIFF_TERM=TRUE")),
+        Subsignal("tx_p", Pins("FIXME"), Misc("DIFF_TERM=TRUE")),
+        Subsignal("tx_n", Pins("FIXME"), Misc("DIFF_TERM=TRUE")),
+        Subsignal("rx_p", Pins("FIXME"), Misc("DIFF_TERM=TRUE")),
+        Subsignal("rx_n", Pins("FIXME"), Misc("DIFF_TERM=TRUE")),
+        IOStandard("LVDS_25"),
+    ),
 
 ]
 
@@ -399,11 +414,134 @@ class DRTIOTestSoC(SoCCore):
             gth.cd_rtio_rx.clk)
 
 
+class AMCRTMLinkControl(Module, AutoCSR):
+    def __init__(self):
+        self._tx_prbs_config = CSRStorage(2)
+
+        self._rx_bitslip_value = CSRStorage(5)
+        self._rx_delay_rst = CSR()
+        self._rx_delay_inc = CSRStorage()
+        self._rx_delay_ce = CSR()
+
+        self._rx_prbs_config = CSRStorage(2)
+        self._rx_prbs_errors = CSRStatus(32)
+
+        # # #
+
+        self.tx_prbs_config = self._tx_prbs_config.storage
+
+        self.rx_bitslip_value = self._rx_bitslip_value.storage
+
+        self.rx_prbs_config = self._rx_prbs_config.storage
+        self.rx_prbs_errors = self._rx_prbs_errors.status
+
+
+class AMCRTMLinkTestSoC(SoCCore):
+    csr_map = {
+        "master_serdes_control": 20,
+        "slave_serdes_control": 21,
+        "analyzer": 22
+    }
+    csr_map.update(SoCCore.csr_map)
+
+    def __init__(self, platform, analyzer=True):
+        clk_freq = int(125e6)
+        SoCCore.__init__(self, platform, clk_freq,
+            cpu_type=None,
+            csr_data_width=32,
+            with_uart=False,
+            ident="Sayma AMC DRTIO Test Design",
+            with_timer=False
+        )
+        self.submodules.crg = _CRG(platform)
+        self.add_cpu_or_bridge(UARTWishboneBridge(platform.request("serial"),
+                                                  clk_freq, baudrate=115200))
+        self.add_wb_master(self.cpu_or_bridge.wishbone)
+
+        self.crg.cd_sys.clk.attr.add("keep")
+        platform.add_period_constraint(self.crg.cd_sys.clk, 8.0)
+
+        master_pll = SERDESPLL(125e6, 1.25e9)
+        self.comb += master_pll.refclk.eq(ClockSignal())
+        self.submodules += master_pll
+
+        master_pads = platform.request("amc_rtm_link")
+        self.submodules.master_serdes = master_serdes = SERDES(
+            master_pll, master_pads, mode="master")
+        self.comb += master_serdes.tx_produce_square_wave.eq(0) # FIXME
+        self.submodules.master_serdes_control = master_serdes_control = AMCRTMLinkControl()
+        self.comb += [
+            master_serdes.tx_prbs.config.eq(master_serdes_control.tx_prbs_config),
+            master_serdes.rx_bitslip_value.eq(master_serdes_control.rx_bitslip_value),
+            master_serdes.rx_prbs.config.eq(master_serdes_control.rx_prbs_config),
+            master_serdes_control.rx_prbs_errors.eq(master_serdes.rx_prbs.errors)
+        ]
+
+        master_serdes.cd_rtio.clk.attr.add("keep")
+        master_serdes.cd_serdes.clk.attr.add("keep")
+        master_serdes.cd_serdes_div.clk.attr.add("keep")
+        platform.add_period_constraint(master_serdes.cd_rtio.clk, 16.0),
+        platform.add_period_constraint(master_serdes.cd_serdes.clk, 1.6),
+        platform.add_period_constraint(master_serdes.cd_serdes_div.clk, 6.4)
+        self.platform.add_false_path_constraints(
+            self.crg.cd_sys.clk,
+            master_serdes.cd_rtio.clk,
+            master_serdes.cd_serdes.clk,
+            master_serdes.cd_serdes_div.clk)
+
+        counter = Signal(32)
+        self.sync.rtio += counter.eq(counter + 1)
+        self.comb += [
+            master_serdes.encoder.d[0].eq(counter),
+            master_serdes.encoder.d[1].eq(counter)
+        ]
+
+        master_sys_counter = Signal(32)
+        self.sync.sys += master_sys_counter.eq(master_sys_counter + 1)
+        #self.comb += platform.request("user_led", 0).eq(master_sys_counter[26]) # FIXME
+
+        master_rtio_counter = Signal(32)
+        self.sync.rtio += master_rtio_counter.eq(master_rtio_counter + 1)
+        #self.comb += platform.request("user_led", 1).eq(master_rtio_counter[26]) # FIXME
+
+        master_serdes_div_counter = Signal(32)
+        self.sync.serdes_div += master_serdes_div_counter.eq(master_serdes_div_counter + 1)
+        #self.comb += platform.request("user_led", 2).eq(master_serdes_div_counter[26]) # FIXME
+
+        master_serdes_counter = Signal(32)
+        self.sync.serdes += master_serdes_counter.eq(master_serdes_counter + 1)
+        #self.comb += platform.request("user_led", 3).eq(master_serdes_counter[26]) # FIXME
+
+        analyzer_signals = [
+            master_serdes.encoder.k[0],
+            master_serdes.encoder.d[0],
+            master_serdes.encoder.output[0],
+            master_serdes.encoder.k[1],
+            master_serdes.encoder.d[1],
+            master_serdes.encoder.output[1],
+
+            master_serdes.decoders[0].input,
+            master_serdes.decoders[0].d,
+            master_serdes.decoders[0].k,
+            master_serdes.decoders[1].input,
+            master_serdes.decoders[1].d,
+            master_serdes.decoders[1].k,
+
+            master_serdes.rx_prbs.errors,
+        ]
+        self.submodules.analyzer = LiteScopeAnalyzer(analyzer_signals, 512, cd="rtio")
+
+    def do_exit(self, vns):
+        if hasattr(self, "analyzer"):
+            self.analyzer.export_csv(vns, "test/analyzer.csv")
+
+
 def main():
     platform = Platform()
     #soc = SDRAMTestSoC(platform)
     #soc = JESDTestSoC(platform)
-    soc = DRTIOTestSoC(platform)
+    #soc = DRTIOTestSoC(platform)
+    soc = AMCRTMLinkTestSoC(platform)
     builder = Builder(soc, output_dir="build_sayma_amc", csr_csv="test/csr.csv")
     vns = builder.build()
 
