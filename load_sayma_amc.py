732f138a8d9fa12b090f997e0ad6fc8faff9fced
Florent Kermarrec <florent@enjoy-digital.fr>
2017-07-05 23:41:17 +0200

    load_sayma_amc: add target

--- load_sayma_amc.py
+++ load_sayma_amc.py
@@ -4,4 +4,4 @@ from litex.build.xilinx import VivadoProgrammer
 prog = VivadoProgrammer()
 prog.load_bitstream(
     bitstream_file="build_sayma_amc/gateware/top.bit",
-    target="FIXME")
+    target="localhost:3121/xilinx_tcf/Digilent/210249810959")
