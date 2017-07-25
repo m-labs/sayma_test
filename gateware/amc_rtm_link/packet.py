4684142459da6c670b336fb681021352ef8db713
Florent Kermarrec <florent@enjoy-digital.fr>
2017-07-25 19:55:13 +0200

    amc_rtm_link: simplify packet (remove crossbar, simplify packetizer, depacketizer), add test_etherbone

--- gateware/amc_rtm_link/packet.py
+++ gateware/amc_rtm_link/packet.py
@@ -1,11 +1,9 @@
 from math import ceil
-from collections import OrderedDict
 
 from litex.gen import *
 from litex.gen.genlib.misc import WaitTimer
 
 from litex.soc.interconnect import stream
-from litex.soc.interconnect.stream import EndpointDescription
 
 
 def reverse_bytes(signal):
@@ -66,108 +64,17 @@ class Header:
                 r.append(field.eq(signal[start:end]))
         return r
 
-
-class Arbiter(Module):
-    def __init__(self, masters, slave):
-        if len(masters) == 0:
-            pass
-        elif len(masters) == 1:
-            self.grant = Signal()
-            self.comb += masters.pop().connect(slave)
-        else:
-            self.submodules.rr = RoundRobin(len(masters))
-            self.grant = self.rr.grant
-            cases = {}
-            for i, master in enumerate(masters):
-                status = Status(master)
-                self.submodules += status
-                self.comb += self.rr.request[i].eq(status.ongoing)
-                cases[i] = [master.connect(slave)]
-            self.comb += Case(self.grant, cases)
-
-
-class Dispatcher(Module):
-    def __init__(self, master, slaves, one_hot=False):
-        if len(slaves) == 0:
-            self.sel = Signal()
-        elif len(slaves) == 1:
-            self.comb += master.connect(slaves.pop())
-            self.sel = Signal()
-        else:
-            if one_hot:
-                self.sel = Signal(len(slaves))
-            else:
-                self.sel = Signal(max=len(slaves))
-
-            # # #
-
-            status = Status(master)
-            self.submodules += status
-
-            sel = Signal.like(self.sel)
-            sel_ongoing = Signal.like(self.sel)
-            self.sync += \
-                If(status.first,
-                    sel_ongoing.eq(self.sel)
-                )
-            self.comb += \
-                If(status.first,
-                    sel.eq(self.sel)
-                ).Else(
-                    sel.eq(sel_ongoing)
-                )
-            cases = {}
-            for i, slave in enumerate(slaves):
-                if one_hot:
-                    idx = 2**i
-                else:
-                    idx = i
-                cases[idx] = [master.connect(slave)]
-            cases["default"] = [master.ready.eq(1)]
-            self.comb += Case(sel, cases)
-
-
-packet_header_length = 12
-packet_header_fields = {
-    "preamble": HeaderField(0,  0, 32),
-    "dst":      HeaderField(4,  0, 32),
-    "length":   HeaderField(8,  0, 32)
-}
-packet_header = Header(packet_header_fields,
-                       packet_header_length,
-                       swap_field_bytes=True)
-
-
 def phy_description(dw):
     layout = [("data", dw)]
-    return EndpointDescription(layout)
+    return stream.EndpointDescription(layout)
 
 
 def user_description(dw):
     layout = [
-        ("data", dw),
-        ("dst",    8),
+        ("data", 32),
         ("length", 32)
     ]
-    return EndpointDescription(layout)
-
-
-class MasterPort:
-    def __init__(self, dw):
-        self.source = stream.Endpoint(user_description(dw))
-        self.sink = stream.Endpoint(user_description(dw))
-
-
-class SlavePort:
-    def __init__(self, dw, tag):
-        self.sink = stream.Endpoint(user_description(dw))
-        self.source = stream.Endpoint(user_description(dw))
-        self.tag = tag
-
-
-class UserPort(SlavePort):
-    def __init__(self, dw, tag):
-        SlavePort.__init__(self, dw, tag)
+    return stream.EndpointDescription(layout)
 
 
 class Packetizer(Module):
@@ -179,45 +86,31 @@ class Packetizer(Module):
 
         # Packet description
         #   - preamble : 4 bytes
-        #   - unused   : 3 bytes
-        #   - dst      : 1 byte
         #   - length   : 4 bytes
         #   - payload
-        header = [
-            # preamble
-            0x5aa55aa5,
-            # dst
-            sink.dst,
-            # length
-            sink.length
-        ]
-
-        header_unpack = stream.Unpack(len(header), phy_description(32))
-        self.submodules += header_unpack
-
-        for i, byte in enumerate(header):
-            chunk = getattr(header_unpack.sink.payload, "chunk" + str(i))
-            self.comb += chunk.data.eq(byte)
 
         fsm = FSM(reset_state="IDLE")
         self.submodules += fsm
 
         fsm.act("IDLE",
             If(sink.valid,
-                NextState("INSERT_HEADER")
+                NextState("INSERT_PREAMBLE")
             )
         )
-
-        fsm.act("INSERT_HEADER",
-            header_unpack.sink.valid.eq(1),
+        fsm.act("INSERT_PREAMBLE",
             source.valid.eq(1),
-            source.data.eq(header_unpack.source.data),
-            header_unpack.source.ready.eq(source.ready),
-            If(header_unpack.sink.ready,
+            source.data.eq(0x5aa55aa5),
+            If(source.ready,
+                NextState("INSERT_LENGTH")
+            )
+        )
+        fsm.act("INSERT_LENGTH",
+            source.valid.eq(1),
+            source.data.eq(sink.length),
+            If(source.ready,
                 NextState("COPY")
             )
         )
-
         fsm.act("COPY",
             source.valid.eq(sink.valid),
             source.data.eq(sink.data),
@@ -237,58 +130,30 @@ class Depacketizer(Module):
 
         # Packet description
         #   - preamble : 4 bytes
-        #   - unused   : 3 bytes
-        #   - dst      : 1 byte
         #   - length   : 4 bytes
         #   - payload
-        preamble = Signal(32)
-
-        header = [
-            # dst
-            source.dst,
-            # length
-            source.length
-        ]
-
-        header_pack = ResetInserter()(stream.Pack(phy_description(32), len(header)))
-        self.submodules += header_pack
-
-        for i, byte in enumerate(header):
-            chunk = getattr(header_pack.source.payload, "chunk" + str(i))
-            self.comb += byte.eq(chunk.data)
 
         fsm = FSM(reset_state="IDLE")
         self.submodules += fsm
 
-        self.comb += preamble.eq(sink.data)
-        fsm.act("IDLE",
-            sink.ready.eq(1),
-            If((sink.data == 0x5aa55aa5) & sink.valid,
-                   NextState("RECEIVE_HEADER")
-            ),
-            header_pack.source.ready.eq(1)
-        )
-
         self.submodules.timer = WaitTimer(clk_freq*timeout)
         self.comb += self.timer.wait.eq(~fsm.ongoing("IDLE"))
 
-        fsm.act("RECEIVE_HEADER",
-            header_pack.sink.valid.eq(sink.valid),
-            header_pack.sink.payload.eq(sink.payload),
-            If(self.timer.done,
-                NextState("IDLE")
-            ).Elif(header_pack.source.valid,
-                NextState("COPY")
-            ).Else(
-                sink.ready.eq(1)
+        fsm.act("IDLE",
+            sink.ready.eq(1),
+            If(sink.valid & (sink.data == 0x5aa55aa5),
+                   NextState("RECEIVE_LENGTH")
             )
         )
-
-        self.comb += header_pack.reset.eq(self.timer.done)
-
+        fsm.act("RECEIVE_LENGTH",
+            sink.ready.eq(1),
+            If(sink.valid,
+                NextValue(source.length, sink.data),
+                NextState("COPY")
+            )
+        )
         last = Signal()
         cnt = Signal(32)
-
         fsm.act("COPY",
             source.valid.eq(sink.valid),
             source.last.eq(last),
@@ -298,7 +163,6 @@ class Depacketizer(Module):
                 NextState("IDLE")
             )
         )
-
         self.sync += \
             If(fsm.ongoing("IDLE"),
                 cnt.eq(0)
@@ -306,60 +170,3 @@ class Depacketizer(Module):
                 cnt.eq(cnt + 1)
             )
         self.comb += last.eq(cnt == source.length[2:] - 1)
-
-
-class Crossbar(Module):
-    def __init__(self):
-        self.users = OrderedDict()
-        self.master = MasterPort(32)
-        self.dispatch_param = "dst"
-
-    def get_port(self, dst):
-        port = UserPort(32, dst)
-        if dst in self.users.keys():
-            raise ValueError("Destination {0:#x} already assigned".format(dst))
-        self.users[dst] = port
-        return port
-
-    def do_finalize(self):
-        # TX arbitrate
-        sinks = [port.sink for port in self.users.values()]
-        self.submodules.arbiter = Arbiter(sinks, self.master.source)
-
-        # RX dispatch
-        sources = [port.source for port in self.users.values()]
-        self.submodules.dispatcher = Dispatcher(self.master.sink,
-                                                sources,
-                                                one_hot=True)
-        cases = {}
-        cases["default"] = self.dispatcher.sel.eq(0)
-        for i, (k, v) in enumerate(self.users.items()):
-            cases[k] = self.dispatcher.sel.eq(2**i)
-        self.comb += \
-            Case(getattr(self.master.sink, self.dispatch_param), cases)
-
-
-class Core(Module):
-    def __init__(self, clk_freq):
-        self.sink = sink = stream.Endpoint(phy_description(32))
-        self.source = source = stream.Endpoint(phy_description(32))
-
-        # # #
-
-        rx_pipeline = [sink]
-        tx_pipeline = [source]
-
-        # depacketizer / packetizer
-        self.submodules.depacketizer = Depacketizer(clk_freq)
-        self.submodules.packetizer = Packetizer()
-        rx_pipeline += [self.depacketizer]
-        tx_pipeline += [self.packetizer]
-
-        # crossbar
-        self.submodules.crossbar = Crossbar()
-        rx_pipeline += [self.crossbar.master]
-        tx_pipeline += [self.crossbar.master]
-
-        # graph
-        self.submodules.rx_pipeline = stream.Pipeline(*rx_pipeline)
-        self.submodules.tx_pipeline = stream.Pipeline(*reversed(tx_pipeline))
